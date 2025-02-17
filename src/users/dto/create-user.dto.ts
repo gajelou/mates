73f26f7ef053a5ecbc9c1aeadccc979bf4173f5d
Guilherme De Jesus <gabrieljelourenco@gmail.com>
2025-02-17 20:19:05 -0300

    Adding class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,26 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 
 export class CreateUserDto {
-
+    
     readonly id: number;
+
+    @IsString()
     readonly name: string;
+
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
     readonly email: string;
+
+    @IsString()
+    @IsNotEmpty()
     readonly password: string;
+
+    @IsString()
     readonly createdAt: string;
+
+    @IsString()
     readonly updatedAt: string;
 
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,11 +1,28 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
     readonly id: number;
+
+    @IsString()
+    @IsNotEmpty()
     readonly name?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
     readonly email?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    @MaxLength(20)
     readonly password?: string;
+
+    @IsString()
     readonly createdAt?: string;
+
+    @IsString()
     readonly updatedAt?: string;
 }
