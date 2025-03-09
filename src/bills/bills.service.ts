4f5e7fc314c3c6e232d78bd7e33b73b1a1c39c71
Guilherme De Jesus <gabrieljelourenco@gmail.com>
2025-03-09 10:04:26 -0300

    fix:bills service.

--- src/bills/bills.service.ts
+++ src/bills/bills.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
+// bills.service.ts
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Bills } from './entities/bill.entity';
 import { CreateBillDto } from './dto/create-bill.dto';
 import { UpdateBillDto } from './dto/update-bill.dto';
 
 @Injectable()
 export class BillsService {
-  create(createBillDto: CreateBillDto) {
-    return 'This action adds a new bill';
+  constructor(
+    @InjectRepository(Bills)
+    private readonly billsRepository: Repository<Bills>
+  ) {}
+
+  async create(createBillDto: CreateBillDto): Promise<Bills> {
+    const newBill = this.billsRepository.create(createBillDto);
+    return await this.billsRepository.save(newBill);
   }
 
-  findAll() {
-    return `This action returns all bills`;
+  async findAll(): Promise<Bills[]> {
+    return await this.billsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} bill`;
+  async findOne(id: number): Promise<Bills> {
+    const bill = await this.billsRepository.findOne({ where: { id } });
+    if (!bill) {
+      throw new NotFoundException(`Conta com ID ${id} não encontrada.`);
+    }
+    return bill;
   }
 
-  update(id: number, updateBillDto: UpdateBillDto) {
-    return `This action updates a #${id} bill`;
+  async update(id: number, updateBillDto: UpdateBillDto): Promise<Bills> {
+    const bill = await this.findOne(id);
+    Object.assign(bill, updateBillDto);
+    return await this.billsRepository.save(bill);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} bill`;
+  async remove(id: number): Promise<{ message: string }> {
+    const bill = await this.findOne(id);
+    await this.billsRepository.delete(id);
+    return { message: `Conta ${id} removida com sucesso.` };
   }
 }
